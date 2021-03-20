eef66a7b7864190bb3885d07e7a9e7c195626c67
SUSHANT GIRDHAR <52759137+sushantgirdhar@users.noreply.github.com>
2021-03-20 08:33:29 +0530

    Update song.py

--- Bot/modules/song.py
+++ Bot/modules/song.py
@@ -18,7 +18,7 @@ def yt_search(song):
         return url
 
 
-@app.on_message(filters.create(ignore_blacklisted_users) & filters.command("song"))
+@app.on_message(filters.command("song"))
 async def song(client, message):
     chat_id = message.chat.id
     user_id = message.from_user["id"]
